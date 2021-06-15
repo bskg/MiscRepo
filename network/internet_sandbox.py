1a7676b552082b48b3006a1ee21efc4ef32b81c9
Scarl3tt <faeriegoat@gmail.com>
2021-06-14 18:03:34 -0700

    check

--- network/internet_sandbox.py
+++ network/internet_sandbox.py
@@ -1,9 +1,7 @@
-#!/usr/bin/env python3
-
 import socket
 
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+HOST = '127.0.0.1'
+PORT = 65432  
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
